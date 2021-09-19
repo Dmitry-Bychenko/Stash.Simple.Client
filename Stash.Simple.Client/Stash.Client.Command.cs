bc2d6fb3e5fa68e0ef8471581f8b1dff1145c8ad
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-09-19 23:34:41 +0300

    Add project files.

---
+++ Stash.Simple.Client/Stash.Client.Command.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stash.Simple.Client {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// BitBucket Query
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class StashQuery {
+    #region Constants
+
+    /// <summary>
+    /// Default Page Size for QueryPagedAsync
+    /// </summary>
+    public const int DEFAULT_PAGE_SIZE = 100;
+
+    #endregion Constants
+
+    #region Private Data
+
+    private int m_DefaultPageSize = DEFAULT_PAGE_SIZE;
+
+    private static readonly Regex s_AddressRegex = new(@"^\s*([\p{L}0-9]*)\s*[;,:]+\s*", RegexOptions.Compiled);
+
+    #endregion Private Data
+
+    #region Algorithm
+
+    private string MakeAddress(string address) {
+      if (string.IsNullOrWhiteSpace(address))
+        return "";
+
+      address = address.Trim('/', ' ');
+
+      if (address.StartsWith("rest/", StringComparison.OrdinalIgnoreCase))
+        return string.Join("/", Connection.Server, address);
+      else {
+        var match = s_AddressRegex.Match(address);
+
+        if (match.Success) {
+          string api = match.Groups[1].Value;
+
+          if (string.IsNullOrWhiteSpace(api))
+            api = "api";
+
+#pragma warning disable IDE0057 // Use range operator
+          return string.Join("/", Connection.Server, $"rest/{api}/latest", address.Substring(match.Index + match.Length).Trim('/', ' '));
+#pragma warning restore IDE0057 // Use range operator
+        }
+        else
+          return string.Join("/", Connection.Server, "rest/api/latest", address);
+      }
+    }
+
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public StashQuery(StashConnection connection) {
+      Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Connection
+    /// </summary>
+    public StashConnection Connection { get; }
+
+    /// <summary>
+    /// Default Page Size
+    /// </summary>
+    public int DefaultPageSize {
+      get => m_DefaultPageSize;
+      set {
+        if (value <= 0 || value >= 1000)
+          throw new ArgumentOutOfRangeException(nameof(value));
+
+        m_DefaultPageSize = value;
+      }
+    }
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query</param>
+    /// <param name="method">Http Method</param>
+    /// <returns></returns>
+    public async Task<(JsonDocument document, string error)> TryQueryAsync(string address, string query, HttpMethod method, CancellationToken token) {
+      if (address is null)
+        throw new ArgumentNullException(nameof(address));
+
+      address = MakeAddress(address);
+
+      //address = string.Join("/",
+      //   Connection.Server.TrimEnd('/'),
+      //  "rest",
+      //  "api",
+      //  "latest",
+      //   address.TrimStart('/'));
+
+      if (address.Contains('?'))
+        address += $"&limit={DefaultPageSize}";
+      else
+        address += $"?limit={DefaultPageSize}";
+
+      query ??= "{}";
+
+      using var req = new HttpRequestMessage {
+        Method = method,
+        RequestUri = new Uri(address),
+        Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+          { HttpRequestHeader.Authorization.ToString(), Connection.Auth},
+        },
+        Content = new StringContent(query, Encoding.UTF8, "application/json")
+      };
+
+      var response = await StashConnection.Client.SendAsync(req, token).ConfigureAwait(false);
+
+      if (!response.IsSuccessStatusCode) {
+        string message = response.ReasonPhrase;
+
+        if (string.IsNullOrWhiteSpace(message))
+          message = $"Failed with code {(int)(response.StatusCode)}: {response.StatusCode}";
+
+        return (null, message);
+      }
+
+      Connection.m_IsConnected = true;
+
+      using Stream stream = await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
+
+      return (await JsonDocument.ParseAsync(stream, default, token).ConfigureAwait(false), null);
+    }
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address">Address</param>
+    /// <param name="query">Query</param>
+    /// <param name="method">Http Method</param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address, string query, HttpMethod method, CancellationToken token) {
+      if (address is null)
+        throw new ArgumentNullException(nameof(address));
+
+      address = MakeAddress(address);
+
+      if (address.Contains('?'))
+        address += $"&limit={DefaultPageSize}";
+      else
+        address += $"?limit={DefaultPageSize}";
+
+      query ??= "{}";
+
+      using var req = new HttpRequestMessage {
+        Method = method,
+        RequestUri = new Uri(address),
+        Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+          { HttpRequestHeader.Authorization.ToString(), Connection.Auth},
+        },
+        Content = new StringContent(query, Encoding.UTF8, "application/json")
+      };
+
+      var response = await StashConnection.Client.SendAsync(req, token).ConfigureAwait(false);
+
+      if (!response.IsSuccessStatusCode) {
+        string message = response.ReasonPhrase;
+
+        if (string.IsNullOrWhiteSpace(message))
+          message = $"Failed with code {(int)(response.StatusCode)}: {response.StatusCode}";
+
+        throw new DataException(message);
+      }
+
+      Connection.m_IsConnected = true;
+
+      using Stream stream = await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
+
+      return await JsonDocument.ParseAsync(stream, default, token).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="query"></param>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address, string query, HttpMethod method) =>
+      await QueryAsync(address, query, method, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="query"></param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address, string query, CancellationToken token) =>
+      await QueryAsync(address, query, HttpMethod.Post, token).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="query"></param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address, string query) =>
+      await QueryAsync(address, query, HttpMethod.Post, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address, CancellationToken token) =>
+      await QueryAsync(address, "", HttpMethod.Get, token).ConfigureAwait(false);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public async Task<JsonDocument> QueryAsync(string address) =>
+      await QueryAsync(address, "", HttpMethod.Get, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Query
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="query"></param>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                                int pageSize,
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      if (address is null)
+        throw new ArgumentNullException(nameof(address));
+
+      if (pageSize <= 0)
+        pageSize = DefaultPageSize;
+
+      address = MakeAddress(address);
+
+      if (address.Contains('?'))
+        address += $"&limit={pageSize}";
+      else
+        address += $"?limit={pageSize}";
+
+      query ??= "{}";
+
+      int start = 0;
+
+      while (start >= 0) {
+        using var req = new HttpRequestMessage {
+          Method = method,
+          RequestUri = new Uri(address + (start == 0 ? "" : $"&start={start}")),
+          Headers = {
+          { HttpRequestHeader.Accept.ToString(), "application/json" },
+          { HttpRequestHeader.Authorization.ToString(), Connection.Auth},
+        },
+          Content = new StringContent(query, Encoding.UTF8, "application/json")
+        };
+
+        var response = await StashConnection.Client.SendAsync(req, token).ConfigureAwait(false);
+
+        if (!response.IsSuccessStatusCode)
+          throw new DataException(string.IsNullOrEmpty(response.ReasonPhrase)
+            ? $"Query failed with {response.StatusCode} ({(int)response.StatusCode}) code"
+            : response.ReasonPhrase);
+
+        Connection.m_IsConnected = true;
+
+        using Stream stream = await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
+
+        var jsonDocument = await JsonDocument.ParseAsync(stream, default, token).ConfigureAwait(false);
+
+        if (jsonDocument.RootElement.TryGetProperty("nextPageStart", out var prop))
+          start = prop.GetInt32();
+        else
+          start = -1;
+
+        yield return jsonDocument;
+      }
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                                int pageSize) {
+      await foreach (var item in QueryPagedAsync(address, query, method, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                int pageSize,
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, query, HttpMethod.Post, pageSize, token))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                int pageSize) {
+      await foreach (var item in QueryPagedAsync(address, query, HttpMethod.Post, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                int pageSize,
+                                                                [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, pageSize, token))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                int pageSize) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, pageSize, CancellationToken.None))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, query, method, -1, token))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                                HttpMethod method) {
+      await foreach (var item in QueryPagedAsync(address, query, method, -1, CancellationToken.None))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, query, HttpMethod.Post, -1, token))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                                string query) {
+      await foreach (var item in QueryPagedAsync(address, query, HttpMethod.Post, -1, CancellationToken.None))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address,
+                                                               [EnumeratorCancellation]
+                                                                CancellationToken token) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, -1, token))
+        yield return item;
+    }
+
+    /// <summary>
+    /// Paged Query 
+    /// </summary>
+    public async IAsyncEnumerable<JsonDocument> QueryPagedAsync(string address) {
+      await foreach (var item in QueryPagedAsync(address, "", HttpMethod.Get, -1, CancellationToken.None))
+        yield return item;
+    }
+
+    #endregion Public
+  }
+
+}
---
+++ Stash.Simple.Client/Stash.Client.Connection.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Data.Common;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stash.Simple.Client {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Stash Connection
+  /// </summary>
+  /// <seealso cref="https://docs.atlassian.com/bitbucket-server/rest/4.0.0-SNAPSHOT/bitbucket-rest.html"/>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class StashConnection : IEquatable<StashConnection> {
+    #region Private Data
+
+    private static readonly CookieContainer s_CookieContainer;
+
+    private static readonly HttpClient s_HttpClient;
+
+    internal bool m_IsConnected;
+
+    #endregion Private Data
+
+    #region Create
+
+    static StashConnection() {
+      try {
+        ServicePointManager.SecurityProtocol =
+          SecurityProtocolType.Tls |
+          SecurityProtocolType.Tls11 |
+          SecurityProtocolType.Tls12;
+      }
+      catch (NotSupportedException) {
+        ;
+      }
+
+      s_CookieContainer = new CookieContainer();
+
+      var handler = new HttpClientHandler() {
+        CookieContainer = s_CookieContainer,
+        Credentials = CredentialCache.DefaultCredentials,
+      };
+
+      s_HttpClient = new HttpClient(handler) {
+        Timeout = Timeout.InfiniteTimeSpan,
+      };
+    }
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public StashConnection(string login, string password, string server) {
+      Login = login ?? throw new ArgumentNullException(nameof(login));
+      Password = password ?? throw new ArgumentNullException(nameof(password));
+      Server = server?.Trim()?.TrimEnd('/') ?? throw new ArgumentNullException(nameof(server));
+
+      Auth = $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Login}:{Password}"))}";
+    }
+
+    // Data Source=http address;User ID=myUsername;password=myPassword;
+    /// <summary>
+    /// Conenction with Connection String 
+    /// </summary>
+    public StashConnection(string connectionString) {
+      if (connectionString is null)
+        throw new ArgumentNullException(nameof(connectionString));
+
+      DbConnectionStringBuilder builder = new() {
+        ConnectionString = connectionString
+      };
+
+      if (builder.TryGetValue("User ID", out var login) &&
+          builder.TryGetValue("password", out var password) &&
+          builder.TryGetValue("Data Source", out var server)) {
+        Login = login?.ToString() ?? throw new ArgumentException("Login not found", nameof(connectionString));
+        Password = password?.ToString() ?? throw new ArgumentException("Password not found", nameof(connectionString));
+        Server = server?.ToString()?.Trim()?.TrimEnd('/') ?? throw new ArgumentException("Server not found", nameof(connectionString));
+
+        Auth = $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Login}:{Password}"))}";
+      }
+      else
+        throw new ArgumentException("Invalid connection string", nameof(connectionString));
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Http Client
+    /// </summary>
+    public static HttpClient Client => s_HttpClient;
+
+    /// <summary>
+    /// Create Query
+    /// </summary>
+    public StashQuery CreateQuery() => new(this);
+
+    /// <summary>
+    /// Is Connected
+    /// </summary>
+    public bool IsConnected => m_IsConnected;
+
+    /// <summary>
+    /// Connect Async
+    /// </summary>
+    public async Task ConnectAsync(CancellationToken token) {
+      if (m_IsConnected)
+        return;
+
+      token.ThrowIfCancellationRequested();
+
+      var q = CreateQuery();
+
+      q.DefaultPageSize = 1;
+
+      using var doc = await q.QueryAsync("users", token).ConfigureAwait(false);
+
+      m_IsConnected = true;
+    }
+
+    /// <summary>
+    /// Connect Async
+    /// </summary>
+    public async Task ConnectAsync() => await ConnectAsync(CancellationToken.None);
+
+    /// <summary>
+    /// Login
+    /// </summary>
+    public string Login { get; }
+
+    /// <summary>
+    /// Password
+    /// </summary>
+    public string Password { get; }
+
+    /// <summary>
+    /// Authentification
+    /// </summary>
+    public string Auth { get; }
+
+    /// <summary>
+    /// Server
+    /// </summary>
+    public string Server { get; }
+
+    /// <summary>
+    /// To String
+    /// </summary>
+    public override string ToString() => $"{Login}@{Server}";
+
+    #endregion Public
+
+    #region IEquatable<BitBucketConnection>
+
+    /// <summary>
+    /// Equals 
+    /// </summary>
+    public bool Equals(StashConnection other) {
+      if (ReferenceEquals(this, other))
+        return true;
+      if (other is null)
+        return false;
+
+      return string.Equals(Login, other.Login) &&
+             string.Equals(Password, other.Password) &&
+             string.Equals(Server, other.Server, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// Equals
+    /// </summary>
+    public override bool Equals(object obj) => obj is StashConnection other && Equals(other);
+
+    /// <summary>
+    /// Get Hash Code
+    /// </summary>
+    public override int GetHashCode() =>
+      Login.GetHashCode() ^
+      Password.GetHashCode() ^
+      Server.GetHashCode(StringComparison.OrdinalIgnoreCase);
+
+    #endregion IEquatable<BitBucketConnection>
+  }
+
+}
